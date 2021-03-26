d5df5326793b7ad83b3590999e6f6921e8850004
eduard damian garcia <yamiyugi43@gmail.com>
2021-03-26 18:48:59 -0500

    nuevo intento

--- src/test/java/co/edu/eafit/protoapp/tests/TestBot.java
+++ src/test/java/co/edu/eafit/protoapp/tests/TestBot.java
@@ -9,24 +9,7 @@ public class TestBot {
     @Test
     public void testCuadrado() {
         Bot bot = new Bot();
-        assertEquals("Hola, como puedo ayudarte?",Saludo(), 0);
+        assertEquals("Hola, como puedo ayudarte?",bot.Saludo(), 0);
     }
 
-    @Test
-    public void testCero() {
-        Bot bot = new Bot();
-        assertEquals(0.0, bot.cuadrado(0.0), 0);
-    }
-
-    @Test
-    public void testUno() {
-        Bot bot = new Bot();
-        assertEquals(1.0, bot.cuadrado(1.0), 0);
-    }
-
-    @Test
-    public void testNegativo() {
-        Bot bot = new Bot();
-        assertEquals(16.0, bot.cuadrado(-4.0), 0);
-    }
 }
