5373351663e5388d12467a3348eae2cf111009da
eduard damian garcia <yamiyugi43@gmail.com>
2021-03-26 16:55:36 -0500

    tests

---
+++ src/test/java/co/edu/eafit/protoapp/tests/TestBot.java
@@ -0,0 +1,32 @@
+package co.edu.eafit.protoapp.tests;
+
+import co.edu.eafit.protoapp.Bot;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestBot {
+
+    @Test
+    public void testCuadrado() {
+        Bot bot = new Bot();
+        assertEquals(25.0, bot.cuadrado(5.0), 0);
+    }
+
+    @Test
+    public void testCero() {
+        Bot bot = new Bot();
+        assertEquals(0.0, bot.cuadrado(0.0), 0);
+    }
+
+    @Test
+    public void testUno() {
+        Bot bot = new Bot();
+        assertEquals(1.0, bot.cuadrado(1.0), 0);
+    }
+
+    @Test
+    public void testNegativo() {
+        Bot bot = new Bot();
+        assertEquals(16.0, bot.cuadrado(-4.0), 0);
+    }
+}
